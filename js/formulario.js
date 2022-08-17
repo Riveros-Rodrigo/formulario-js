fa26dfe1117e71048529cb0c9cc9ac4dfe88d8ef
Rodrigo Riveros <riverosdev@gmail.com>
2022-08-17 00:37:56 -0300

    agrego expresiones

--- js/formulario.js
+++ js/formulario.js
@@ -3,44 +3,80 @@ const inputs = document.querySelectorAll('#formulario input')
 
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	nombre: /^[a-zA-ZÀ-ÿ\s]{4,16}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
+const campos = {
+	usuario: false,
+	nombre: false,
+	password: false,
+	correo: false,
+	telefono: false
+}
+
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "usuario":
-			if(expresiones.usuario.test(e.target.value)){
-				document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto'); 
-				document.getElementById('grupo__usuario').classList.add('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
-				document.querySelector('#grupo__usuario i').classList.add('fa-check-circle'); //agrego el icono de correcto
-				document.querySelector('#grupo__usuario i').classList.remove('fa-times-circle'); //elimino el icono de incorrecto
-				document.querySelector('#grupo__usuario .formulario__input-error').classList.remove('formulario__input-error-activo');
-			} else{
-				document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto'); //llamo estilos incorrectos que hice en el css
-				document.getElementById('grupo__usuario').classList.remove('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
-				document.querySelector('#grupo__usuario i').classList.add('fa-times-circle'); //agrego el icono de incorrecto
-				document.querySelector('#grupo__usuario i').classList.remove('fa-check-circle'); //elimino el icono de correcto
-				document.querySelector('#grupo__usuario .formulario__input-error').classList.add('formulario__input-error-activo');
-			}
+			validarCampo(expresiones.usuario, e.target, 'usuario');
 		break;
 		case "nombre":
-			
+			validarCampo(expresiones.nombre, e.target, 'nombre');
 		break;
 		case "password":
-			
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
 		break;
 		case "password2":
-			
+			validarPassword2();
 		break;
 		case "correo":
-			
+			validarCampo(expresiones.correo, e.target, 'correo');
 		break;
 		case "telefono":
-			
+			validarCampo(expresiones.telefono, e.target, 'telefono');
 		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto'); 
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle'); //agrego el icono de correcto
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle'); //elimino el icono de incorrecto
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else{
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto'); //llamo estilos incorrectos que hice en el css
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle'); //agrego el icono de incorrecto
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle'); //elimino el icono de correcto
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto'); //llamo estilos incorrectos que hice en el css
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle'); //agrego el icono de incorrecto
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle'); //elimino el icono de correcto
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[password] = false;
+	} else{
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto'); //llamo estilos incorrectos que hice en el css
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto'); //llamo estilos correctos que hice en el css
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle'); //agrego el icono de incorrecto
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle'); //elimino el icono de correcto
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[password] = true;
 	}
 }
 
@@ -51,4 +87,20 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault() //para que no se mande el formulario
+
+	const terminos = document.getElementById('terminos');
+	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000 ); //para que el mensaje del formulario dure 5 seg
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto'); //elimino los iconos al completar formulario
+		});
+	} else{
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
 });
